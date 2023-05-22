9a236e126e559b88d6ab9d17040d3c5d78d1db24
robertkosciuszko <114183493+robertkosciuszko@users.noreply.github.com>
2023-05-22 16:25:21 +0200

    uppdate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -33,16 +33,7 @@ export const App = () => {
   };
 
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
+    <div  className="container"
     >
       <Section title="Please leave feedback">
         <FeedbackOptions
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -13,16 +13,4 @@ Section.propTypes = {
   children: propTypes.node.isRequired,
 };
 
-export const FirstOption = ({ props }) => (
-  <div>
-    <h1>Opcja bez return</h1>
-  </div>
-);
 
-export const SecondOption = ({ props }) => {
-  return (
-    <div>
-      <h1>Opcja z return</h1>
-    </div>
-  );
-};
